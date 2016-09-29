97239865a0bae5b2621b3b09947255d44a49ef11
Ihor Gorobets <ihorgorobets@gmail.com>
2016-09-29 21:33:01 +0300

    Add chart

--- countries/views.py
+++ countries/views.py
@@ -3,12 +3,16 @@ from django.shortcuts import get_list_or_404, render
 
 from countries.models import Country, Region
 
+
 def index(request):
-    return render(request, 'countries/index.html')
+    return render(request, 'countries/index.html', dict(regions=Region.objects.all()))
+
 
 def regions(request):
     return JsonResponse({region.id: region.name for region in Region.objects.all()})
 
 
 def region(request, id):
-    return JsonResponse({country.id: country.name for country in get_list_or_404(Country, region_id=id)})
+    return JsonResponse([{ 'country': country.name , 'value': country.value}
+        for country in get_list_or_404(Country,
+        region_id=id)], safe=False)
